1bba670c6cdce6c1a6675d3f82fe9a2e4f1e8bc6
That1LinuxGuy <mcallen71398@gmail.com>
2026-06-21 15:04:13 -0500

    migrated kde to it's own module

--- hosts/pikachu/pikachu.nix
+++ hosts/pikachu/pikachu.nix
@@ -29,16 +29,6 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-  
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver = {
-    enable = false;
-  };
-
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
---
+++ modules/plasma/plasma.nix
@@ -0,0 +1,14 @@
+{ pkgs, config, ... }:
+
+# KDE Plasma configuration
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = false;
+  services.desktopManager.plasma6.enable = true;
+  
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver = {
+    enable = false;
+  };
+
