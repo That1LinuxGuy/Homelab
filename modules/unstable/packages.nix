8dd94987dfc7c66d90780cdbc9b25184aa4853e6
That1LinuxGuy <mcallen71398@gmail.com>
2026-07-01 21:46:07 -0500

    updated system

--- modules/unstable/packages.nix
+++ modules/unstable/packages.nix
@@ -1,7 +1,7 @@
 { pkgs, self, nixpkgs-unstable, ... }: 
 
 let
-  unstable = nixpkgs-unstable.legacyPackages.${system};
+  unstable = nixpkgs-unstable.legacyPackages.x86_64-linux;
 in
 {
   environment.systemPackages = with pkgs; [
