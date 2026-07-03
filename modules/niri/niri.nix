13911438482d50095d6e6ddfc2b573a3b533be04
That1LinuxGuy <mcallen71398@gmail.com>
2026-07-03 14:15:26 -0500

    added inputs.noctalia to noctalia

--- modules/niri/niri.nix
+++ modules/niri/niri.nix
@@ -12,7 +12,7 @@
   programs.niri.enable = true;
   
   environment.systemPackages = with pkgs; [
-  noctalia.legacyPackages.${pkgs.system}.default
+  inputs.noctalia.legacyPackages.${pkgs.system}.default
   xwayland-satellite
   alacritty
   fuzzel
