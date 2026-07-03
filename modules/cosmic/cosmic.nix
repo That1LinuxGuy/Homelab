18d43a00e8b895f874e519d20be1cde14842fdfb
That1LinuxGuy <mcallen71398@gmail.com>
2026-07-02 22:50:23 -0500

    enable wayland in sddm

--- modules/cosmic/cosmic.nix
+++ modules/cosmic/cosmic.nix
@@ -6,7 +6,12 @@
   # Enable the Cosmic Desktop Environment.
   services.displayManager.sddm = {
     enable = true;
+    wayland.enable = true;
     theme = "sddm-astronaut-theme";
     extraPackages = [pkgs.sddm-astronaut];
   services.desktopManager.cosmic.enable = true;
+
+  environment.systemPackages = with pkgs; [
+  sddm-astronaut
+  ];
 }
--- modules/niri/niri.nix
+++ modules/niri/niri.nix
@@ -5,6 +5,7 @@
 {
   services.displayManager.sddm = {
     enable = true;
+    wayland.enable = true;
     theme = "sddm-astronaut-theme";
     extraPackages = [pkgs.sddm-astronaut];
   };
