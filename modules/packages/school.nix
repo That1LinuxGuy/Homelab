24bcb14a861690f0e7f3390f60fbf9887c996bfe
That1LinuxGuy <mcallen71398@gmail.com>
2026-08-09 23:55:28 -0500

    fix school packages

--- modules/packages/school.nix
+++ modules/packages/school.nix
@@ -1,7 +1,8 @@
-# List of child-friendly learning materials
+## List of child-friendly learning materials
 
 { config, pkgs, ... }:
 
+{
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
