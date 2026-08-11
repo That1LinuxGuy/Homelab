34a45fb531677ac6d07dd859e38920a0568879ab
mcallen <mcallen71398@gmail.com>
2026-08-11 14:42:28 -0500

    changed cac card module

--- modules/cac-card/cac.nix
+++ modules/cac-card/cac.nix
@@ -26,38 +26,38 @@
   };
 
   # Install and enable chromium
-  programs.chromium = {
-    enable = true;
-  };
-
-  # Setup the local database for chromium-based browsers
-  systemd.user.services.setup-chromium-cac = {
-    description = "Register CAC Module in Chromium NSS DB";
-    wantedBy = [ "default.target" ];
-    
-    # 1. Define as a one-shot process
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true; # Marks service as active so systemd won't re-run it
-    };
-
-    path = [ pkgs.nss pkgs.coreutils ];
-    
-    script = ''
-      NSSDB="$HOME/.pki/nssdb"
-      
-      if [ -f "$NSSDB/cert9.db" ] && modutil -dbdir "sql:$NSSDB" -list | grep -q "CAC Module"; then
-        exit 0
-      fi
-
-      mkdir -p "$NSSDB"
-
-      if [ ! -f "$NSSDB/cert9.db" ]; then
-        modutil -create -dbdir "sql:$NSSDB" -force
-      fi
-
-      modutil -dbdir "sql:$NSSDB" -add "CAC Module" -libfile "${pkgs.opensc}/lib/opensc-pkcs11.so" -force
-    '';
-  };
-  
-}
+#  programs.chromium = {
+#    enable = true;
+#  };
+#
+#  # Setup the local database for chromium-based browsers
+#  systemd.user.services.setup-chromium-cac = {
+#    description = "Register CAC Module in Chromium NSS DB";
+#    wantedBy = [ "default.target" ];
+#    
+#    # 1. Define as a one-shot process
+#    serviceConfig = {
+#      Type = "oneshot";
+#      RemainAfterExit = true; # Marks service as active so systemd won't re-run it
+#    };
+#
+#    path = [ pkgs.nss pkgs.coreutils ];
+#    
+#    script = ''
+#      NSSDB="$HOME/.pki/nssdb"
+#      
+#      if [ -f "$NSSDB/cert9.db" ] && modutil -dbdir "sql:$NSSDB" -list | grep -q "CAC Module"; then
+#        exit 0
+#      fi
+#
+#      mkdir -p "$NSSDB"
+#
+#      if [ ! -f "$NSSDB/cert9.db" ]; then
+#        modutil -create -dbdir "sql:$NSSDB" -force
+#      fi
+#
+#      modutil -dbdir "sql:$NSSDB" -add "CAC Module" -libfile "${pkgs.opensc}/lib/opensc-pkcs11.so" -force
+#    '';
+#  };
+#  
+#}
