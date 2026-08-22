eb13125565a2bfa86bf980c08f1b2e929fb9c672
mcallen <mcallen71398@gmail.com>
2026-08-22 14:23:37 -0500

    clean up fingerprint.nix

--- modules/common/fingerprint.nix
+++ modules/common/fingerprint.nix
@@ -4,8 +4,6 @@
   # Enable fingerprint authentication service
   services.fprintd = {
     enable = true;
-    # Optional: explicitly set libfprint TOD (Touch OEM Drivers) if using proprietary driver forks
-    # package = pkgs.fprintd-tod; 
   };
 
   # Optional: Automatically prompt for fingerprint in PAM (sudo, login, lockscreen)
