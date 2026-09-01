67d6b4f618c2ab5a8981720789e2a2084102e908
mcallen <mcallen71398@gmail.com>
2026-08-31 20:17:54 -0500

    add tools packages

---
+++ modules/packages/tools.nix
@@ -0,0 +1,11 @@
+## List of helpful tools for laptops
+
+{ config, pkgs, ... }:
+
+{
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    localsend
+  ];
+}
