f146ed3b22e3a547ff1dac1ffa886088b2b1c06c
mcallen <mcallen71398@gmail.com>
2026-09-01 20:54:36 -0500

    update tools

--- modules/packages/tools.nix
+++ modules/packages/tools.nix
@@ -3,12 +3,10 @@
 { config, pkgs, ... }:
 
 {
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     localsend
-    converseen
-    kdePackages.gwenview
-    thunar
+    photoqt
+    switcheroo
+    yazi
   ];
 }
