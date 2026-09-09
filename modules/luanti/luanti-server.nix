16f6355fa96b42c7445cb9b5ba1335504f80f089
That1LinuxGuy <mcallen71398@gmail.com>
2026-09-08 23:25:47 -0500

    fixed minetest server

--- modules/luanti/luanti-server.nix
+++ modules/luanti/luanti-server.nix
@@ -6,22 +6,12 @@
   services.minetest-server = {
     enable = true;
     port = 30000;
-    gameId = "minetest_game";
+    gameId = "mineclonia";
+
     config = {
       server_name = "Allen Family";
-      max_users = 10;
       creative_mode = false;
       enable_damage = true;
-      
-      package = pkgs.minetest-server.override {
-        extraGames = [
-          pkgs.minetestgames.mineclonia
-          pkgs.minetestgames.minetest_game
-        ];
-        extraMods = [
-        # pkgs.minetestmods.mobs_monster
-        ];
-      };
     };
   };
 }
