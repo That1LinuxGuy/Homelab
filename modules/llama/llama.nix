78370cad084d051ff00416e67f2daa7516dfa9c6
mcallen <mcallen71398@gmail.com>
2026-09-09 22:43:18 -0500

    llama-cpp-vulkan

--- modules/llama/llama.nix
+++ modules/llama/llama.nix
@@ -3,8 +3,12 @@
 { config, pkgs, ... }:
 
 {
+  environment.systemPackages = with pkgs; [
+    llama-cpp-vulkan
+  ];
+
   services.llama-cpp= {
     enable = true;
-    package = pkgs.llama-cpp-vulkan:
+    package = pkgs.llama-cpp-vulkan;
   };
 }
