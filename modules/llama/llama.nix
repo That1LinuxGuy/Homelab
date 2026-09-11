6c04087d3032919ee170bfa026f735f18f0d8af2
mcallen <mcallen71398@gmail.com>
2026-09-10 23:12:19 -0500

    add llama-server settings

--- modules/llama/llama.nix
+++ modules/llama/llama.nix
@@ -7,8 +7,17 @@
     llama-cpp-vulkan
   ];
 
-  services.llama-cpp= {
+  services.llama-cpp = {
     enable = true;
     package = pkgs.llama-cpp-vulkan;
+    settings = {
+      model = "/var/lib/models/Qwen3.6-35B-A3B-UD-IQ4_XS.gguf";
+      host = "127.0.0.1";
+      port = 8080;
+      n-gpu-layers = 99;
+      threads = 2;
+      ctx-size = 16384;
+      flash-attn = "on";
+    };
   };
 }
