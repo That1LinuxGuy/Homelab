95f3ae4f904bc5deac9426e027134d46c77e639c
mcallen <mcallen71398@gmail.com>
2026-09-14 11:10:50 -0500

    add search locally to laptops

---
+++ modules/common/search.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+# Add searxng to laptops for AI use cases
+
+{  
+  # Enable searxng
+  services.searx = {
+    enable = true;
+    package = pkgs.searxng; 
+    redisCreateLocally = true; 
+    settings = {
+      server.bind_address = "127.0.0.1";
+      server.port = 8888;
+      server.secret_key = "@SEARXNG_SECRET@";
+
+      search = {
+        safe_search = 0;
+        autocomplete = "duckduckgo"
+        formats = [
+          "html"
+          "json"
+        ];
+      };
+
+      engines = [
+        {name = "google"; disabled = false; }
+        {name = "duckduckgo"; disabled = false; }
+      ];
+    };
+  };
+}
